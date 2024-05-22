69e2d3ca403ab80e9ec3948bed9915d8d627a587
mjzk <mj@mjzk.xyz>
2024-05-22 16:15:54 +0800

    CHORE: cleanings

--- src/eth/gas.rs
+++ src/eth/gas.rs
@@ -27,21 +27,11 @@ async fn gas_price_usd() -> ChainXResult<()> {
     let rpc_url = "https://mainnet.infura.io/v3/c60b0bb42f8a4c6481ecd229eddaca27";
     let provider = ProviderBuilder::new().on_builtin(rpc_url).await?;
 
-    // let call = latestAnswerCall {}.abi_encode();
-    // let input = Bytes::from(call);
-
-    // let tx = TransactionRequest::default()
-    //     .with_to(ETH_USD_FEED)
-    //     .with_input(input);
-
-    // let response = provider.call(&tx).block(BlockId::latest()).await?;
-    // let result = U256::from_str(&response.to_string())?;
-
     let wei_per_gas = provider.get_gas_price().await?;
     let result = get_price_u256(provider, ETH_USD_FEED).await?;
-    // Convert the gas price to Gwei and USD.
+
     let gwei = format_units(wei_per_gas, "gwei")?.parse::<f64>()?;
-    let usd = get_usd_value(wei_per_gas, result)?;
+    let usd = get_gas_usd_value(wei_per_gas, result)?;
 
     println!("Gas price in Gwei: {gwei}");
     println!("Gas price in USD: {usd}");
@@ -83,22 +73,7 @@ fn result_to_f64_value(value: U256, decimals: u32) -> ChainXResult<f64> {
     Ok(value_f64 / scale)
 }
 
-// async fn l2_prices() -> Result<(), Box<dyn std::error::Error>> {
-//     // Create a provider
-//     let rpc_url = format!("https://mainnet.infura.io/v3/{}", INFURA_PROJECT_ID);
-//     let provider = ProviderBuilder::new().http(&rpc_url).await?;
-
-//     // Get the prices for L2 solutions
-//     let arbitrum_price = get_price(&provider, ARBITRUM_USD_FEED).await?;
-//     let optimism_price = get_price(&provider, OPTIMISM_USD_FEED).await?;
-
-//     println!("Arbitrum price in USD: {}", arbitrum_price);
-//     println!("Optimism price in USD: {}", optimism_price);
-
-//     Ok(())
-// }
-
-fn get_usd_value(amount: u128, price_usd: U256) -> ChainXResult<f64> {
+fn get_gas_usd_value(amount: u128, price_usd: U256) -> ChainXResult<f64> {
     let base = U256::from(10).pow(U256::from(ETH_DECIMALS));
     let value = U256::from(amount) * price_usd / base;
     let formatted = format_units(value, ETH_USD_FEED_DECIMALS)?.parse::<f64>()?;
